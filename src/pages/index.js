7b18b5ec9af72eac640d620d6c997a353a08a0f7
Crash2022 <crash555@mail.ru>
2023-02-04 21:48:57 +0500

    add: dynamic links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import {Link, graphql} from 'gatsby';
 import {StaticImage, GatsbyImage, getImage} from 'gatsby-plugin-image';
-
 import Layout from '../components/layout';
 import Seo from '../components/seo';
+
 // import * as styles from '../components/index.module.css';
 
 /*const links = [
--- src/templates/single-post.js
+++ src/templates/single-post.js
@@ -1,19 +1,48 @@
 import * as React from 'react';
-import {Link} from 'gatsby';
-
+import {Link, graphql} from 'gatsby';
+import {GatsbyImage, getImage} from 'gatsby-plugin-image';
 import Layout from '../components/layout';
 import Seo from '../components/seo';
 
-const SinglePost = () => (
-    <Layout>
-        {/*<Seo title="Post 1"/>*/}
-        <div>
-            <h1>Post 1</h1>
-        </div>
-        <Link to="/">Go back to the homepage</Link>
-    </Layout>
-);
+const SinglePost = ({data}) => {
 
-export const Head = () => <Seo title="Post 1"/>;
+    const { html } = data.markdownRemark
+    const { title, url, category, image } = data.markdownRemark.frontmatter
+    const img = getImage(image);
+
+    return (
+        <Layout>
+            <Seo title={title}/>
+            <div>
+                <h1>{title}</h1>
+            </div>
+            <div>
+                <GatsbyImage alt={title} image={img}/>
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: html }}/>
+            <Link to="/">Go back to the homepage</Link>
+        </Layout>
+    );
+};
+
+// export const Head = () => <Seo title={title}/>;
 
 export default SinglePost;
+
+export const query = graphql(`
+    query PostQuery($url: String) {
+        markdownRemark(frontmatter: {url: {eq: $url}}) {
+            html
+                frontmatter {
+                    title
+                    url
+                    category
+                    image {
+                    childImageSharp {
+                        gatsbyImageData(width: 600)
+                    }
+                }
+            }
+        }
+    }
+`);
